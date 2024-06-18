2835189598c7722f5fc435b0e34494a3916b6e01
MhlengiNgubane <mhlengims@gmail.com>
2024-06-18 09:11:45 +0200

    first commit

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple_interest = (principal * rate * time) / 100
+simple_interest = (principal * rate * time)
 
-print(f"Simple interest is : {simple_interest:.2f}")
+print(f"Simple interest is : {simple_interest:.1f}")

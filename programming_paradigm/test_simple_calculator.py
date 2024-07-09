2c7c6afdb0129a13773dafa978114ff81a6633eb
MhlengiNgubane <mhlengims@gmail.com>
2024-07-09 10:16:05 +0200

    test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -23,6 +23,10 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the multiply method."""
         self.assertEqual(self.calc.multiply(3, 2), 6)
         self.assertEqual(self.calc.multiply(1, -1), -1)
-        self.assertEqual(self.calc.multiply(5, 0), 0)    
-
-# Remember to write additional test methods for subtract, multiply, and divide.
+        self.assertEqual(self.calc.multiply(5, 0), 0)
+        
+    def test_division(self):
+        """Test the divide method."""
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(1, -1), -1)
+        self.assertEqual(self.calc.divide(5, 0), None)       
